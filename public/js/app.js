e9f85de0d8719bb5c114f11533a2d50c5ca1ac2d
yash11213018 <yash.bhanushali@sakec.ac.in>
2020-05-29 19:17:56 +0530

    bar plot update

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javascript file is loaded!')
-
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
@@ -15,11 +13,24 @@ const description = document.querySelector('.desc')
 const pressure = document.querySelector('.press')
 const precipitation = document.querySelector('.preci')
 const wind = document.querySelector('.speed')
+const icon = document.querySelector('#mainImage')
 const WeatherCard = document.querySelector('#weatherCardDisplay')
+const footer=document.querySelector('footer')
 
-
+/* -----------dat section ---- */
+var d = new Date()
+var today =d.getUTCDate()
+var prev = today-7
+var month = d.getUTCMonth() + 1; // Since getUTCMonth() returns month from 0-11 not 1-12
+var year = d.getUTCFullYear();
+var dateStr = today + "-" + month + "-" + year;
+var startStr = prev + "-" + month + "-" + year;
 
 WeatherCard.style.display="none"; //hide weather card before trigger action
+
+
+
+
 function stringManip({CurrentTemperature,WeatherDescription,WeatherHumidity,WeatherPressure}={}) {
     
     messageTwo=''
@@ -52,12 +63,35 @@ weatherForm.addEventListener('submit', (e) => {
                 wind.textContent=""+data.forecast.WindSpeed
                 pressure.textContent="Pressure : "+data.forecast.WeatherPressure
                 precipitation.textContent=""+data.forecast.WindPrecip
+                footer.style.top = "0" //resetting the footer to original place.
                 WeatherCard.style.display="block" //show data card after data rendered.
-
-
+                icon.src=''+data.forecast.WeatherIcon              
                 
+                var ctx = document.getElementById('myChart').getContext('2d');
+
+                var chart = new Chart(ctx, {
+                        // The type of chart we want to create
+                   type: 'line',
+
+                      // The data for our dataset
+                   data: {
+                           labels: ['Temperature', 'WindSpeed', 'Pressure', 'Precipitation', 'Humidity', 'Visibility'],
+                          datasets: [{
+                                   label: 'Temperature Attributes',
+                                   backgroundColor: 'rgb(255, 99, 132)',
+                                   borderColor: 'rgb(255, 99, 132)',
+                                   data: [data.forecast.CurrentTemperature, data.forecast.WindSpeed, data.forecast.WeatherPressure, data.forecast.WeatherPressure,data.forecast.humidity,data.forecast.Visibility]
+                         }]
+                    },
+                     // Configuration options go here
+                  options: {}
+                  }
+
+                  );
+                  chart.update()
 
             }
         })
     })
 })
+
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,8 +1,15 @@
 const request = require('request')
-
+/* -----------date section ---- */
+var d = new Date()
+var today =d.getUTCDate()
+var prev = today-7
+var month = d.getUTCMonth() + 1; // Since getUTCMonth() returns month from 0-11 not 1-12
+var year = d.getUTCFullYear();
+var dateStr = today + "-" + month + "-" + year;
+var startStr = prev + "-" + month + "-" + year;
 const forecast = (latitude, longitude, callback) => {
-    const url="http://api.weatherstack.com/current?access_key=9e5cb44eab18c6700c358c51e3cb8f22&query="+ longitude + ',' + latitude
-
+    const url="http://api.weatherstack.com/current?access_key=9e5cb44eab18c6700c358c51e3cb8f22&query="+ longitude + ',' + latitude +""
+    
     request({ url, json: true }, (error, { body } ) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
@@ -14,7 +21,10 @@ const forecast = (latitude, longitude, callback) => {
             "WeatherHumidity":body.current.humidity,
             "WeatherPressure":body.current.pressure,
             "WindSpeed":body.current.wind_speed,
-            "WindPrecip":body.current.precip
+            "WindPrecip":body.current.precip,
+            "WeatherIcon":body.current.weather_icons[0],
+            "Visibility":body.current.Visibility
+
         })
         }
     })
